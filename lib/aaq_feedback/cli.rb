6b96b857042df3bdc84710882e89b3d878c84ca2
ruthmesfun <ruth.mesfun@gmail.com>
2017-11-27 08:04:25 -0500

    Added a frequency choice. Right now shows the % of positive comments per type.

--- lib/aaq_feedback/cli.rb
+++ lib/aaq_feedback/cli.rb
@@ -15,16 +15,17 @@ class AaqFeedback::CLI
     input = gets.strip.to_i
 
     if input == 1
-      puts " Would you like 1. All data, 2. Average rating"
+      puts " Would you like 1. All data, 2. Average rating, 3.Frequency"
       input = gets.strip.to_i
 
       AaqFeedback::Report.find_and_create_from_survey
 
       if input == 1
-
         puts AaqFeedback::Report.tokens
       elsif input == 2
         puts AaqFeedback::Report.average_rating
+      elsif input == 3
+        puts AaqFeedback::Report.frequency_of_positive_feedback
       end
     elsif input == 2
       puts "Technical Coach"
