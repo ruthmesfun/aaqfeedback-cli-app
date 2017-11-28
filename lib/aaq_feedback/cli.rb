7ea00affe9fdef0c38620b8cf2612aeac9032cce
ruthmesfun <ruth.mesfun@gmail.com>
2017-11-28 18:25:39 -0500

    changed the frequency methods and average ratings methods to add a month argument and added a question to input month

--- lib/aaq_feedback/cli.rb
+++ lib/aaq_feedback/cli.rb
@@ -15,37 +15,55 @@ class AaqFeedback::CLI
     input = gets.strip.to_i
 
     if input == 1
-      puts " Would you like 1. All data, 2. Average rating, 3.Frequency, 4. All of the above, 5. End"
-      input = gets.strip.to_i
+      while input > 0
+        puts " Would you like 1. All data, 2. Average rating, 3.Frequency, 4. All of the above, 5. End"
+        input = gets.strip.to_i
 
-      AaqFeedback::Report.find_and_create_from_survey
-      case input
-      when 1
-        puts AaqFeedback::Report.tokens
-      when 2
-        puts AaqFeedback::Report.average_rating
-      when 3
-        puts "POSITIVE FEEDBACK \n-----------------"
-        puts AaqFeedback::Report.frequency_of_positive_feedback
-        puts "\n \n "
-        puts "NEGATIVE FEEDBACK \n-----------------"
-        puts AaqFeedback::Report.frequency_of_negative_feedback
-      when 4
-        puts "AVERAGE RATING \n-----------------"
-        puts AaqFeedback::Report.average_rating
-        puts "POSITIVE FEEDBACK \n-----------------"
-        puts AaqFeedback::Report.frequency_of_positive_feedback
-        puts "\n \n "
-        puts "NEGATIVE FEEDBACK \n-----------------"
-        puts AaqFeedback::Report.frequency_of_negative_feedback
-      when 5
-        puts "Thank you for trying out the Technical Coach Feedback App!"
+        AaqFeedback::Report.find_and_create_from_survey
+          case input
+          when 1
+            puts AaqFeedback::Report.tokens
+            puts "\n to end this app type 0"
+            input = gets.strip.to_i
+          when 2
+            puts "What month would you like an to choose from? 1-12"
+            month = gets.strip
+            puts AaqFeedback::Report.average_rating(month)
+            puts "\n to end this app type 0"
+            input = gets.strip.to_i
+          when 3
+            puts "What month would you like an to choose from? 1-12"
+            month = gets.strip
+
+            puts "POSITIVE FEEDBACK \n-----------------"
+            puts AaqFeedback::Report.frequency_of_positive_feedback(month)
+            puts "\n \n "
+            puts "NEGATIVE FEEDBACK \n-----------------"
+            puts AaqFeedback::Report.frequency_of_negative_feedback(month)
+            puts "\n to end this app type 0"
+            input = gets.strip.to_i
+          when 4
+            puts "What month would you like an to choose from? 1-12"
+            month = gets.strip
+
+            puts "AVERAGE RATING \n-----------------"
+            puts AaqFeedback::Report.average_rating(month)
+            puts "POSITIVE FEEDBACK \n-----------------"
+            puts AaqFeedback::Report.frequency_of_positive_feedback(month)
+            puts "\n \n "
+            puts "NEGATIVE FEEDBACK \n-----------------"
+            puts AaqFeedback::Report.frequency_of_negative_feedback(month)
+            puts "\n to end this app type 0"
+            input = gets.strip.to_i
+          end
+
+          puts "Thank you for trying out the Technical Coach Feedback App!"
+        end
+      elsif input == 2
+        puts "Technical Coach"
+      else
+        puts "I don't understand that choice."
+        menu
       end
-    elsif input == 2
-      puts "Technical Coach"
-    else
-      puts "I don't understand that choice."
-      menu
     end
-  end
 end
