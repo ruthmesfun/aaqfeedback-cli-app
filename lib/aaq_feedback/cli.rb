87234a647f8b951e5eb23cfd52691122ee30b028
ruthmesfun <ruth.mesfun@gmail.com>
2017-11-27 22:00:45 -0500

    Added more choices in the menu method

--- lib/aaq_feedback/cli.rb
+++ lib/aaq_feedback/cli.rb
@@ -15,17 +15,31 @@ class AaqFeedback::CLI
     input = gets.strip.to_i
 
     if input == 1
-      puts " Would you like 1. All data, 2. Average rating, 3.Frequency"
+      puts " Would you like 1. All data, 2. Average rating, 3.Frequency, 4. All of the above, 5. End"
       input = gets.strip.to_i
 
       AaqFeedback::Report.find_and_create_from_survey
-
-      if input == 1
+      case input
+      when 1
         puts AaqFeedback::Report.tokens
-      elsif input == 2
+      when 2
         puts AaqFeedback::Report.average_rating
-      elsif input == 3
+      when 3
+        puts "POSITIVE FEEDBACK \n-----------------"
         puts AaqFeedback::Report.frequency_of_positive_feedback
+        puts "\n \n "
+        puts "NEGATIVE FEEDBACK \n-----------------"
+        puts AaqFeedback::Report.frequency_of_negative_feedback
+      when 4
+        puts "AVERAGE RATING \n-----------------"
+        puts AaqFeedback::Report.average_rating
+        puts "POSITIVE FEEDBACK \n-----------------"
+        puts AaqFeedback::Report.frequency_of_positive_feedback
+        puts "\n \n "
+        puts "NEGATIVE FEEDBACK \n-----------------"
+        puts AaqFeedback::Report.frequency_of_negative_feedback
+      when 5
+        puts "Thank you for trying out the Technical Coach Feedback App!"
       end
     elsif input == 2
       puts "Technical Coach"
