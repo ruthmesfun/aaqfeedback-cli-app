33be4f3dd16c9c01612e1f428d2e5e380503cf5e
ruthmesfun <ruth.mesfun@gmail.com>
2017-11-29 11:14:37 -0500

    Created average rating method for technical coaches

--- lib/aaq_feedback/report.rb
+++ lib/aaq_feedback/report.rb
@@ -56,25 +56,26 @@ class AaqFeedback::Report
       #feedback response
       survey["answers"]. each do |key, value|
         # positive feedback response
-        list_name = "list_z3PoGEKivPJy_choice_"
+        positive_list_name = "list_z3PoGEKivPJy_choice_"
+        negative_list_name = "list_AOFdoBaHgkUy_choice_"
 
         case key #add a loop?
-        when list_name + "w5gBmbDDFw8U"
+        when positive_list_name + "w5gBmbDDFw8U"
           token.positive_feedback << "guide me while I debug my code"
-        when list_name + "hZUNhYcKu2YU"
+        when positive_list_name + "hZUNhYcKu2YU"
           token.positive_feedback << "Boosted my confidence in my coding debugging skills"
-        when list_name + "Es8ECIWFTZ7a"
+        when positive_list_name + "Es8ECIWFTZ7a"
           token.positive_feedback << "I was able to speak to them immediately and have a screenshare quickly"
-        when list_name + "DXnkiNkeYsDl"
+        when positive_list_name + "DXnkiNkeYsDl"
           token.positive_feedback<< "Explain concepts clearly and concisely"
         # negative feedback response
-        when list_name + "poNMdtvx7lHi"
+        when negative_list_name + "poNMdtvx7lHi"
           token.negative_feedback << "Ask me more questions"
         when "list_AOFdoBaHgkUy_choice_oHXicFgyGi2h"
           token.negative_feedback << "Encourage me"
-        when list_name + "kNj37gv2WRgU"
+        when negative_list_name + "kNj37gv2WRgU"
           token.negative_feedback<< "Decrease waittime"
-        when list_name + "Oqvv5HXi5kyK"
+        when negative_list_name + "Oqvv5HXi5kyK"
           token.negative_feedback << "Explain concepts clearly and share resources"
         end
       end
@@ -83,8 +84,15 @@ class AaqFeedback::Report
   end
 
   def self.monthly_feedback(month)
-    tokens_from_month = self.tokens.select{|token| token.date.include?("2017-#{month}")}
+    tokens_from_month = []
+    self.tokens.select do |token|
+      tokens_from_month << token if token.date.include?("2017-#{month}")
+    end
     tokens_from_month
+  end
+
+  def self.num_monthly_feedback(month)
+    monthly_feedback(month).count
   end
   #----- RATINGS ----#
   def self.average_rating(month)
@@ -99,7 +107,7 @@ class AaqFeedback::Report
 
   def self.all_positive_feedback(month)
     all_positive_feedback = []
-      self.monthly_feedback(month).select do |token|
+    self.monthly_feedback(month).select do |token|
       token.positive_feedback.each{|feedback| all_positive_feedback << feedback}
     end
 
@@ -114,12 +122,12 @@ class AaqFeedback::Report
   def self.frequency_of_positive_feedback(month)
     self.all_positive_feedback(month)
 
-    "Guide me while I debug my code: #{((@guide_me.to_i / num_tokens.to_f)*100).round(2)}% \n Boosted my confidence in my codin/debugging skills: #{((@confidence / num_tokens.to_f)*100).round(2)}% \n I was able to speak to them immediately and have a screenshare quickly: #{((@immediate/num_tokens.to_f)*100).round(2)}% \n Explain concepts clearly and concisely: #{((@concise_concepts / num_tokens.to_f)*100).round(2)}%"
+    "Guide me while I debug my code: #{((@guide_me.to_i / num_monthly_feedback(month).to_f)*100).round(2)}% \n Boosted my confidence in my codin/debugging skills: #{((@confidence / num_monthly_feedback(month).to_f)*100).round(2)}% \n I was able to speak to them immediately and have a screenshare quickly: #{((@immediate/num_monthly_feedback(month).to_f)*100).round(2)}% \n Explain concepts clearly and concisely: #{((@concise_concepts / num_monthly_feedback(month).to_f)*100).round(2)}%"
   end
 
   def self.all_negative_feedback(month)
     all_negative_feedback = []
-      self.monthly_feedback(month).select do |token|
+    self.monthly_feedback(month).select do |token|
       token.negative_feedback.each{|feedback| all_negative_feedback << feedback}
     end
 
@@ -134,7 +142,27 @@ class AaqFeedback::Report
   def self.frequency_of_negative_feedback(month)
     self.all_negative_feedback(month)
 
-    "Ask me more questions: #{((@ask_more.to_i / num_tokens.to_f)*100).round(2)}% \n Encourage me: #{((@encourage_me / num_tokens.to_f)*100).round(2)}% \n Decrease waittime: #{((@decrease_waittime/num_tokens.to_f)*100).round(2)}% \n Explain concepts clearly and share resources: #{((@explain_concepts / num_tokens.to_f)*100).round(2)}%"
+    "Ask me more questions: #{((@ask_more/num_monthly_feedback(month).to_f)*100).round(2)}% \n Encourage me: #{((@encourage_me / num_monthly_feedback(month).to_f)*100).round(2)}% \n Decrease waittime: #{((@decrease_waittime/num_monthly_feedback(month).to_f)*100).round(2)}% \n Explain concepts clearly and share resources: #{((@explain_concepts / num_monthly_feedback(month).to_f)*100).round(2)}%"
   end
+
+  #----FOR TECHNICAL COACHES----#
+  def self.technical_coach_average_rating(month, technical_coach)
+    #REMINDER = need to add date range in this
+    technical_coach_feedback = []
+    tokens_from_month = []
+    ratings = []
+    self.tokens.select do |token|
+      technical_coach_feedback << token if token.technical_coaches.include?(technical_coach)
+    end
+
+    technical_coach_feedback.select do |token|
+      tokens_from_month << token if token.date.include?("2017-#{month}")
+    end
+
+    tokens_from_month.select{|token| ratings << token.rating}
+    average = ratings.inject{ |sum, el| sum + el }.to_f / ratings.size
+    average
+  end
+
 
 end
